4c06e3a5bfe900db706a908b094e8ea90f826a99
Franco Galluccio <francoivangalluccio@gmail.com>
2022-11-01 20:09:50 -0300

    Version 2.5

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay: true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true,
+    responsive:{
+        300:{
+            items:1
+        },
+        550:{
+            items:2
+        },
+        800:{
+            items:3
+        },
+        1000:{
+            items:3
+        }
+    }
+})
